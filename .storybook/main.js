d6dbec520b4374559a8925efcb33bc9347c0c974
Ran Zabaro <ran.zabaro@Ran-Zabaros-Mac.local>
2025-07-15 10:34:54 +0300

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,1 @@
+module.exports = { stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'], addons: ['@storybook/addon-essentials', '@storybook/addon-docs'], framework: '@storybook/react', };
---
+++ src/Button.stories.js
@@ -0,0 +1,12 @@
+import { Button } from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+};
+
+export const Primary = {
+  args: {
+    label: 'Hello Storybook',
+  },
+};
